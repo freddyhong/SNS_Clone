e2f61e961e81714a6403709c5af63f0e7cec2fad
wongi hong <wghong22@gmail.com>
2024-08-25 23:24:04 +0900

    ts ignore

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -14,6 +14,7 @@ import FormInput from "@/app/components/form";
 import FormBtn from "@/app/components/form-btn";
 
 export default function Home() {
+  // @ts-ignore
   const [state, action] = useFormState(handleform, null);
   console.log(state);
   return (
